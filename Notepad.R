eedcddc0fd50ed41f41aab27604d7c3ee59ad118
kateaj99 <kate.a.johnson@gmail.com>
2020-04-05 14:44:44 +0100

    Updated notebook

--- Notepad.R
+++ Notepad.R
@@ -1,1 +1,12 @@
 #Notepad
+
+#Keeps only unique responses
+Q2dataunique <- Q2data %>% distinct(ResponseNoPunc, .keep_all = TRUE)
+summary(Q2dataunique)
+
+#Save out the duplicates to add in later (do this after tokenisation)
+Q2dataduplicates <- Q2data %>% find_duplicates(ResponseNoPunc)
+
+Q2dataduplicates <- Q2dataduplicates %>%
+  group_by(ResponseNoPunc) %>%
+  slice(-1)
